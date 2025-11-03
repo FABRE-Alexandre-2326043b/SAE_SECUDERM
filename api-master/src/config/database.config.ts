a67fc5eb04a55bfb8467fab9ea9c64cca20b9b70
Alexandre <alexandre.fabre.2@etu.univ-amu.fr>
2025-11-03 12:13:58 +0100

    ajout de db neon

--- api-master/src/config/database.config.ts
+++ api-master/src/config/database.config.ts
@@ -1,8 +1,29 @@
 import { SequelizeModuleOptions } from '@nestjs/sequelize';
-import configuration from './configuration';
+import configuration from './configuration'; 
 import { Dialect } from 'sequelize';
 
 export const databaseConfig = (): SequelizeModuleOptions => {
+  if (process.env.DATABASE_URL) {
+    const dbUrl = new URL(process.env.DATABASE_URL);
+
+    return {
+      dialect: 'postgres',
+      host: dbUrl.hostname,
+      port: parseInt(dbUrl.port, 10),
+      username: dbUrl.username,
+      password: dbUrl.password,
+      database: dbUrl.pathname.slice(1), 
+      autoLoadModels: true,
+      synchronize: true, 
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false, 
+        },
+      },
+    };
+  }
+
   const config = configuration();
   return {
     dialect: config.database.dialect as Dialect,
